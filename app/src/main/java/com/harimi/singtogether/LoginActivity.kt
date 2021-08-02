ebe9e2779cfe8da6535b7a1e87c308d3fdc8c96f
hiharim <kyj96604@naver.com>
2021-08-02 19:22:28 +0900

    앱설치테스트

---
+++ app/src/main/java/com/harimi/singtogether/LoginActivity.kt
@@ -0,0 +1,74 @@
+package com.harimi.singtogether
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.kakao.sdk.auth.LoginClient
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ApiErrorCause
+import com.kakao.sdk.common.model.AuthErrorCause
+import com.kakao.sdk.common.model.AuthErrorCause.*
+import com.kakao.sdk.common.util.Utility
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+//        val keyHash= Utility.getKeyHash(this)
+//        Log.d("키해쉬값",keyHash)
+
+
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                when {
+                    error.toString() == AccessDenied.toString() -> {
+                        Toast.makeText(this, "접근이 거부 됨(동의 취소)", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == InvalidClient.toString() -> {
+                        Toast.makeText(this, "유효하지 않은 앱", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == InvalidGrant.toString() -> {
+                        Toast.makeText(this, "인증 수단이 유효하지 않아 인증할 수 없는 상태", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == InvalidRequest.toString() -> {
+                        Toast.makeText(this, "요청 파라미터 오류", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == InvalidScope.toString() -> {
+                        Toast.makeText(this, "유효하지 않은 scope ID", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == Misconfigured.toString() -> {
+                        Toast.makeText(this, "설정이 올바르지 않음(android key hash)", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == ServerError.toString() -> {
+                        Toast.makeText(this, "서버 내부 에러", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == Unauthorized.toString() -> {
+                        Toast.makeText(this, "앱이 요청 권한이 없음", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> { // Unknown
+                        Toast.makeText(this, "기타 에러", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else if (token != null) {
+                Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+
+            }
+        }
+
+        activity_login_btn_login_kakao.setOnClickListener {
+            if(LoginClient.instance.isKakaoTalkLoginAvailable(this)){
+                LoginClient.instance.loginWithKakaoTalk(this, callback = callback)
+            }else{
+                LoginClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+        }
+    }
+}
--- app/src/main/java/com/harimi/singtogether/MainActivity.kt
+++ app/src/main/java/com/harimi/singtogether/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.harimi.singtogether
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentHome : HomeFragment = HomeFragment() // 홈
+    private val fragmentSing : SingFragment = SingFragment() // 부르기
+    private val fragmentBroadcast : BraodcastFragment=BraodcastFragment() // 방송
+    private val fragmentMyPage : MyPageFragment = MyPageFragment() // 마이페이지
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null){
+            chipNavigationBar.setItemSelected(R.id.home,true)
+
+        }
+        chipNavigationBar.setOnItemSelectedListener { id ->
+            when(id) {
+                R.id.home -> changeFragment(fragmentHome)
+                R.id.sing-> changeFragment(fragmentSing)
+                R.id.live -> changeFragment(fragmentBroadcast)
+                R.id.setting -> changeFragment(fragmentMyPage)
+            }
+        }
+
+    }
+
+    private fun changeFragment(fragment :Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.activity_main_frame,fragment)
+            .commit()
     }
 }
