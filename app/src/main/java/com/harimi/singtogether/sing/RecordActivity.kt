9681b4aafee24131e70ecfcd0d7a1884f2d2f0dc
hiharim <kyj96604@naver.com>
2021-08-12 03:48:05 +0900

    manifest 수정/ record액티비티 수정

--- app/src/main/java/com/harimi/singtogether/sing/RecordActivity.kt
+++ app/src/main/java/com/harimi/singtogether/sing/RecordActivity.kt
@@ -8,11 +8,22 @@ import com.harimi.singtogether.databinding.ActivityRecordBinding
 class RecordActivity: AppCompatActivity()  {
 
     private lateinit var binding: ActivityRecordBinding
+    private var idx : Int? = null
+    private var title : String? = null
+    private var singer : String? = null
+    private var song_path : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        idx=intent.getIntExtra("RECORD_IDX",0)
+        title=intent.getStringExtra("RECORD_TITLE")
+        singer=intent.getStringExtra("RECORD_SINGER")
+        song_path=intent.getStringExtra("RECORD_SONG_PATH")
+
+        binding.toolbarRecord.setBackgroundColor(resources.getColor(R.color.dark_purple))
 
     }
 
