30e570cfad9662ac8fedb8f2de9d1081abc6bb75
tjrgusdlaek <69791042+tjrgusdlaek@users.noreply.github.com>
2021-10-25 13:31:45 +0900

    추가기능 레이아웃 , 액티비티 추가

---
+++ app/src/main/java/com/harimi/singtogether/LookAtUserProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.harimi.singtogether
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LookAtUserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_look_at_user_profile)
+    }
+}
